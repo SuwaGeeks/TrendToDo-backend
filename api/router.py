0ae751a53ff669b3fe32138c38623f75ea357b8c
mochi-yu <mochi.2225.it@gmail.com>
2023-05-21 02:23:04 +0900

    add: Content-Typeの設定を追加

--- api/router.py
+++ api/router.py
@@ -175,4 +175,5 @@ def after_request(response):
   # response.headers.add('Access-Control-Allow-Origin', '*')
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  response.headers['Content-Type'] = 'application/json'
   return response
