03f1b80d2ab93fd53955188345e762e72c0bc4af
mochi-yu <mochi.2225.it@gmail.com>
2023-06-19 22:37:45 +0900

    feat: 新しいユーザが追加されたら、その情報をFirestoreに保存するエンドポイントを追加 #2

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,12 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import { AddNewUserController } from "./controller/AddNewUserController";
 admin.initializeApp();
 
 // ライフチェック用のエンドポイント
 exports.helloWorld = functions.https.onRequest((req, res) => {
   res.send("Hello Firebase!");
 });
+
+// ユーザ作成時に呼ばれるエンドポイント
+exports.addNewUser = functions.auth.user().onCreate( AddNewUserController )
