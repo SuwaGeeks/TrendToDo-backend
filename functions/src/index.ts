41e38cbac8b294f3a06dd9ce0ecf8381cd494a71
mochi-yu <mochi.2225.it@gmail.com>
2023-06-19 22:37:07 +0900

    fix: ライフチェック用のエンドポイントの記述を修正

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 admin.initializeApp();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello Firebase!");
+// ライフチェック用のエンドポイント
+exports.helloWorld = functions.https.onRequest((req, res) => {
+  res.send("Hello Firebase!");
 });
