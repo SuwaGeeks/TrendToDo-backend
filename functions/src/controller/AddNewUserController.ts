238a51f039f0b3a092ff3a9ebb925ea106310248
mochi-yu <mochi.2225.it@gmail.com>
2023-06-21 01:24:06 +0900

    feat: AddUserTaskのエンドポイントを実装 #6

--- functions/src/controller/AddNewUserController.ts
+++ functions/src/controller/AddNewUserController.ts
@@ -2,10 +2,10 @@ import * as admin from "firebase-admin";
 
 export const AddNewUserController = (user: admin.auth.UserRecord) => {
   // コレクションの参照を取得
-  const userRef = admin.firestore().collection('users');
+  const userCollectionRef = admin.firestore().collection('users');
 
   // ユーザテーブルにログインした人の情報を追加
-  userRef.doc(user.uid).set({
+  userCollectionRef.doc(user.uid).set({
     'email': user.email,
     'displayName': user.displayName,
     'creationTime': user.metadata.creationTime,
