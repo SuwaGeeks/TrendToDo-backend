cef7ee3fdc4718bc0680efdfd2ebb55146fd43cb
mochi-yu <mochi.2225.it@gmail.com>
2023-06-21 19:24:17 +0900

    feat: CreateGroupのエンドポイントを実装 #8

--- functions/src/controller/group/CreateGroupController.ts
+++ functions/src/controller/group/CreateGroupController.ts
@@ -1,5 +1,24 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import { GroupModel } from "../../model/GroupModel";
 
 export const CreateGroupController = async (req: functions.https.Request, res: functions.Response<any>) => {
- return ;
+  var status = 200;
+
+  // リクエスト内容の精査
+  if(!req.body.groupName) status = 400;
+
+  // 新しいグループの作成
+  const groupsCollectionRef = admin.firestore().collection('groups');
+  const addGroupResult = await groupsCollectionRef.add({groupName: req.body.groupName});
+
+  // レスポンスデータの作成
+  const createdGroup: GroupModel = {
+    groupId: addGroupResult.id,
+    groupName: req.body.groupName,
+  }
+
+  if(status == 400) res.status(400).send("グループ名が入力されていません");
+  else if(status == 200) res.json({createdGroup: createdGroup});
+  else res.status(400).send("不明なエラーです");
 }
