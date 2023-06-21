080c7c1f86ad55c6dfffbe9aaa7791f6168da7a3
mochi-yu <mochi.2225.it@gmail.com>
2023-06-21 19:24:36 +0900

    feat: GetGroupListのエンドポイントを実装 #8

--- functions/src/controller/group/GetGroupListController.ts
+++ functions/src/controller/group/GetGroupListController.ts
@@ -1,5 +1,25 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-export const GetGroupListController = async (req: functions.https.Request, res: functions.Response<any>) => {
- return ;
+import { GroupModel } from "../../model/GroupModel";
+
+export const SubmitUserTaskController = async (req: functions.https.Request, res: functions.Response<any>) => {
+  // 全てのグループを取得
+  const groups = await admin.firestore().collection('groups').get();
+
+  // グループ一覧を作成
+  var responseData: any[] = [];
+  groups.forEach(elm => {
+    var group: GroupModel = {
+      groupId: elm.id,
+      groupName: elm.get('groupName')
+    }
+    responseData.push(group);
+  })
+
+  if(responseData.length != 0) {
+    res.json({groups: responseData});
+  } else {
+    res.status(404).send('グループがありません');
+  }
 }
